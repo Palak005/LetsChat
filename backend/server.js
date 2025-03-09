33f8b369154cef5c40e3fa4dda324147efd83a3a
Palak <paraj2602@gmail.com>
2025-03-09 14:35:47 +0530

    Project Completed for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./utils/connectDB.js";
@@ -5,18 +6,23 @@ import authRouter from "./routers/auth.routes.js";
 import messageRouter from "./routers/message.routes.js";
 import userRouter from "./routers/user.routes.js";
 import cookieParser from "cookie-parser";
-
 import {app, server} from "./socket/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, ()=>{
     connectDB();
